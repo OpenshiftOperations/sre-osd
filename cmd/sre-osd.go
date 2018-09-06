07937c2ff8c02c25b2bd8cb8bdeba6da255a6479
Taylor Fahlman <tfahlman@redhat.com>
2018-09-06 13:47:08 -0400

    Move create logic out of main binary

--- cmd/sre-osd.go
+++ cmd/sre-osd.go
@@ -3,16 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
-	apiv1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
+	"github.com/openshiftoperations/sre-osd/pkg/srek8s"
+	//"io/ioutil"
+	//apiv1 "k8s.io/api/core/v1"
+	//rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
+	//"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	"log"
 	"path/filepath"
-	"reflect"
+	//"reflect"
 )
 
 var clusterFlag = flag.String("cluster", "", "Specify the name of the cluster")
@@ -55,33 +56,5 @@ func main() {
 
 	clientset := loadKubeConfig()
 
-	//Decode yaml file
-	yamlFile, _ := ioutil.ReadFile(*configFlag)
-	decode := scheme.Codecs.UniversalDeserializer().Decode
-	obj, _, err := decode([]byte(yamlFile), nil, nil)
-	checkErr(err)
-
-	switch o := obj.(type) {
-	case *apiv1.Namespace:
-		fmt.Println("Creating namespace...")
-		_, err = clientset.CoreV1().Namespaces().Create(o)
-		checkErr(err)
-	case *apiv1.ServiceAccount:
-		fmt.Println("Creating serviceaccount...")
-		fmt.Println(o.Namespace)
-		_, err = clientset.CoreV1().ServiceAccounts(o.Namespace).Create(o)
-		checkErr(err)
-	case *rbacv1.ClusterRole:
-		fmt.Println("Creating ClusterRole...")
-		_, err = clientset.RbacV1().ClusterRoles().Create(o)
-		checkErr(err)
-	case *rbacv1.ClusterRoleBinding:
-		fmt.Println("Creating ClusterRoleBinding...")
-		_, err = clientset.RbacV1().ClusterRoleBindings().Create(o)
-		checkErr(err)
-	default:
-		fmt.Println("K8s object not currently handled")
-		fmt.Println(reflect.TypeOf(o))
-		fmt.Println(o)
-	}
+	srek8s.Create(clientset, *configFlag)
 }
